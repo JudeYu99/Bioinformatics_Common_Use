e5fb84d7917861a89387674960106dcd6196bae6
Yu Zhu <1830416012@stu.suda.edu.cn>
2022-04-10 10:36:10 +0800

    Update get_PPIN_from_String.R

--- get_PPIN_from_String.R
+++ get_PPIN_from_String.R
@@ -1,11 +1,12 @@
-# Get PPIN input from String DB (Homo sapiens)
-# Annotation file from ensemble
+# Get PPIN (Gene name) input from String DB (Homo sapiens)
 
+# Annotation file from ensembl. (http://uswest.ensembl.org/biomart/martview)
 ENSP <- read.table("mart_export.txt", header = TRUE, sep = ",")
 ENSP_filt <- ENSP
 ENSP_filt[ENSP_filt == ""] <- NA
 ENSP_filt <- na.omit(ENSP_filt)
 
+# PPIN input with a threshold of combined score >= 400.
 PPI <- read.table("PPI_400.txt", sep = " ", header = T)
 
 temp1 <- data.frame(cbind(paste("9606.", ENSP_filt[,1], sep = "")), ENSP_filt[,2])
@@ -22,6 +23,7 @@ temp4 <- merge(temp3, temp2, by = "protein2")
 temp4 <- temp4[, -1]
 colnames(temp4)[3] <- "protein2"
 
+# Output the PPIN with gene name annotation.
 PPI_result <- cbind(temp4[, 2:3], temp4[, 1])
 colnames(PPI_result)[3] <- "combined.score"
 write.table(PPI_result, file = "PPI_input.csv", quote = F, row.names = F, sep = "\t")
